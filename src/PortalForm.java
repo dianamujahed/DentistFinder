d40b6862cd3b93fe961f95a66bd929a53c367b26
Safaa_mujahed <safamojahed123@gmail.com>
2019-08-27 02:01:57 +0300

    portal form and special form is done

---
+++ src/PortalForm.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+
+public class PortalForm {
+    private JPanel panel1;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JTextField textField7;
+    private JTextField textField8;
+    private JTextField textField9;
+    private JTextField textField10;
+    private JTextField textField11;
+    private JTextField textField12;
+    private JTextField textField13;
+    private JButton addMoreButton;
+    private JTextField textField14;
+    private JTextField textField15;
+    private JTextField textField16;
+    private JTextField textField17;
+    private JButton addMoreButton1;
+}
---
+++ src/SpecialityForm.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class SpecialityForm {
+    private JCheckBox isSpecialistCheckBox;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton addMoreButton;
+}
