e74b65b687931450e9c30232a94eceb06448d8e8
Safaa_mujahed <safamojahed123@gmail.com>
2019-08-27 23:58:50 +0300

    update builder abd dentisBuidler in form

--- src/DentistBuilder.java
+++ src/DentistBuilder.java
@@ -1,54 +1,51 @@
 public class DentistBuilder implements Builder {
     private Dentist dentist;
 
-    public DentistBuilder ()
-    {
+    public DentistBuilder() {
         this.reset();
     }
     // The reset method clears the object being built.
-    public void reset()
-    {
-        this.dentist=new Dentist();
+    public void reset() {
+        this.dentist = new Dentist();
     }
-    public void setEducation(Education education){
+    public void setEducation(Education education) {
         dentist.setEducation(education);
     }
-    public void setAddress(Address address){
+    public void setAddress(Address address) {
         dentist.setAddress(address);
     }
-    public void setSpecialization(Specialization specialization){
+    public void setSpecialization(Specialization specialization) {
         dentist.setSpecialization(specialization);
     }
-    public void setFirstName(String firstName){
+    public void setFirstName(String firstName) {
         dentist.setFirstName(firstName);
     }
-    public void setLastName(String lastName)
-    {
+    public void setLastName(String lastName) {
         dentist.setLastName(lastName);
     }
-    public void setEmailAddress(String emailAddress){
+    public void setEmailAddress(String emailAddress) {
         dentist.setEmailAddress(emailAddress);
     }
-    public void setWebsite(String website){
+    public void setWebsite(String website) {
         dentist.setWebSite(website);
     }
-    public void setPhoneNumber(String phoneNumber){
+    public void setPhoneNumber(String phoneNumber) {
         dentist.setPhoneNumber(phoneNumber);
     }
-    public void setLicenseCountryName(String licenseCountryName){
+    public void setLicenseCountryName(String licenseCountryName) {
         dentist.setLicenseCountryName(licenseCountryName);
     }
-    public void setLicenceNumber(String licenceNumber){
+    public void setLicenceNumber(String licenceNumber) {
         dentist.setLicenseNumber(licenceNumber);
     }
-    public void setYearOfPractice(String yearsInPractice){
+    public void setYearOfPractice(String yearsInPractice) {
         dentist.setYearsInPractice(yearsInPractice);
     }
-    public void setGender(String gender){
+    public void setGender(String gender) {
 
     }
-    public Dentist getResult(){
-        Dentist FinalDentist=this.dentist;
+    public Dentist getResult() {
+        Dentist FinalDentist = this.dentist;
         this.reset();
         return FinalDentist;
 
