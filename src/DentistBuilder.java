40efd78d130b30aa85f414a18a5818034c92ad8f
Safaa_mujahed <safamojahed123@gmail.com>
2019-08-27 23:22:37 +0300

    DentistBuilder class is ready.

---
+++ src/DentistBuilder.java
@@ -0,0 +1,49 @@
+public class DentistBuilder implements Builder {
+    private Dentist dentist;
+
+    public DentistBuilder ()
+    {
+        this.reset();
+    }
+    // The reset method clears the object being built.
+    public void reset()
+    {
+        this.dentist=new Dentist();
+    }
+    public void setFirstName(String firstName){
+        dentist.setFirstName(firstName);
+    }
+    public void setLastName(String lastName)
+    {
+        dentist.setLastName(lastName);
+    }
+    public void setEmailAddress(String emailAddress){
+        dentist.setEmailAddress(emailAddress);
+    }
+    public void setWebsite(String website){
+        dentist.setWebSite(website);
+    }
+    public void setPhoneNumber(String phoneNumber){
+        dentist.setPhoneNumber(phoneNumber);
+    }
+    public void setLicenseCountryName(String licenseCountryName){
+        dentist.setLicenseCountryName(licenseCountryName);
+    }
+    public void setLicenceNumber(String licenceNumber){
+        dentist.setLicenseNumber(licenceNumber);
+    }
+    public void setYearOfPractice(String yearsInPractice){
+        dentist.setYearsInPractice(yearsInPractice);
+    }
+    public void setGender(String gender){
+
+    }
+    public Dentist getResult(){
+        Dentist FinalDentist=this.dentist;
+        this.reset();
+        return FinalDentist;
+
+    }
+
+
+}
