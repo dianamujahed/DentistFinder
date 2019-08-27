8d3dabaa0feaef9a0e83ebe2b4f3855b4e7527f6
Safaa_mujahed <safamojahed123@gmail.com>
2019-08-27 21:34:11 +0300

    specialization class is ready.

---
+++ src/Specialization.java
@@ -0,0 +1,33 @@
+/*
+this class contain the specilalization information for the person.
+ */
+public class Specialization {
+    private String AreaOfSpecialization;
+    private int StratYear;
+    /*
+    a constructor for specialization class.
+     */
+    public Specialization(String areaOfSpecialization,int startYear)
+    {
+        this.AreaOfSpecialization=areaOfSpecialization;
+        this.StratYear=startYear;
+    }
+/*
+a setter and getter functions for specialization class
+ */
+    public void setAreaOfSpecialization(String areaOfSpecialization) {
+        AreaOfSpecialization = areaOfSpecialization;
+    }
+
+    public void setStratYear(int stratYear) {
+        StratYear = stratYear;
+    }
+
+    public int getStratYear() {
+        return StratYear;
+    }
+
+    public String getAreaOfSpecialization() {
+        return AreaOfSpecialization;
+    }
+}
