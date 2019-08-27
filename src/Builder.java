ab43618150a6412578db713858df20b9a1921629
Safaa_mujahed <safamojahed123@gmail.com>
2019-08-27 22:14:17 +0300

    Builder interface is ready.

--- src/Builder.java
+++ src/Builder.java
@@ -1,3 +1,17 @@
-public class  Builder{
+/*
+we use a builder design pattern to make the dentist class more simple.
+so first we create an interface called builder, and the builder specifies methods
+for the dentist class parts.
+ */
+interface Builder{
+    public void setFirstName(String firstName);
+    public void setLastName(String lastName);
+    public void setEmailAddress(String emailAddress);
+    public void setWebsite(String website);
+    public void setPhoneNumber(String phoneNumber);
+    public void setLicienceContryName(String licienceContryName);
+    public void setLicenceNumber(String licenceNumber);
+    public void setYearOfPractice(String yearOfPractice);
+    public void setGender(String gender);
 
 }
